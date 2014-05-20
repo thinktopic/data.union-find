6e4181198800470c35595baf05e1e4642f40df6b
gdevanla <grdvnl@gmail.com>
2014-05-19 17:16:22 -0700

    Add transient support

--- src/jordanlewis/data/union_find.clj
+++ src/jordanlewis/data/union_find.clj
@@ -11,9 +11,16 @@ sets that x and y belong to unioned.")
   (get-canonical [this x] "Get the canonical element of an element, or nil if no
 element exists in the forest."))
 
+(defprotocol TransientDisjointSetForest
+  "A data structure that maintains information on a number of disjoint sets."
+  (union! [this x y] "Union two sets. Return a mutated disjoint set forest with the
+sets that x and y belong to unioned."))
+
 (defrecord ^:private UFNode [value rank parent])
 
 (declare empty-union-find)
+
+(declare ->TransientDSF)
 
 (deftype PersistentDSF [elt-map num-sets _meta]
   Object
@@ -35,6 +42,11 @@ element exists in the forest."))
       (PersistentDSF. (ref (assoc @elt-map x (->UFNode x 0 nil))) (inc num-sets) _meta)))
   (empty [this] empty-union-find)
   (equiv [this that] (.equals this that))
+
+ clojure.lang.IEditableCollection
+  (asTransient [this]
+    (->TransientDSF
+     (transient elt-map) num-sets _meta))
 
   ;; count returns the number of disjoint sets, not the number of total elements
   clojure.lang.Counted
@@ -103,6 +115,86 @@ element exists in the forest."))
                       (ref))
                     num-sets _meta))))
   (count-sets [this] num-sets))
+
+(deftype TransientDSF [elt-map
+                       ^:unsynchronized-mutable num-sets
+                       meta]
+  Object
+  ;; prints out a map from canonical element to elements unioned to that element.
+  (toString [this] (str (group-by this (keys elt-map))))
+
+  clojure.lang.Counted
+  (count [this] num-sets)
+
+  clojure.lang.ILookup
+  ;; valAt gets the canonical element of the key without path compression
+  (valAt [this k] (.valAt this k nil))
+  (valAt [this k not-found]
+    (loop [x k]
+      (if-let [node (elt-map x)]
+        (if-let [parent (:parent node)]
+          (recur parent)
+          x)
+        not-found)))
+
+  clojure.lang.IFn
+  ;; invoking as function behaves like valAt.
+  (invoke [this k] (.valAt this k))
+  (invoke [this k not-found] (.valAt this k not-found))
+
+  clojure.lang.ITransientCollection
+  (conj [this x]
+    (if (elt-map x)
+      this
+      (do (assoc! elt-map x (->UFNode x 0 nil))
+          (set! num-sets (inc num-sets))
+          this)
+      ))
+
+  (persistent [this]
+    (PersistentDSF. (persistent! elt-map) num-sets meta))
+
+  DisjointSetForest
+  (get-canonical [this x]
+    (let [node (elt-map x)
+          parent (:parent node)]
+      (cond
+       (= node nil) [this nil]
+       (= parent nil) [this x]
+       ;; path compression. set the parent of each node on the path we take
+       ;; to the root that we find.
+       :else (let [[_ canonical] (get-canonical this parent)
+                   ]
+               (do (assoc! elt-map x (assoc (get elt-map x) :parent canonical))
+                   [this canonical])))))
+  (union [this x y]
+    (throw (java.lang.UnsupportedOperationException "Use union! on transients")))
+
+  TransientDisjointSetForest
+  (union! [this x y]
+    (let [[_ x-root] (get-canonical this x)
+          [_ y-root] (get-canonical this y)
+          x-rank (:rank (elt-map x-root))
+          y-rank (:rank (elt-map y-root))]
+      (cond (or (nil? x-root) ;; no-op - either the input doesn't exist in the
+                (nil? y-root) ;; universe, or the two inputs are already unioned
+                (= x-root y-root)) this
+            (< x-rank y-rank)
+            (do
+              (assoc! elt-map x-root (assoc (get elt-map x-root) :parent y-root))
+              (set! num-sets (- num-sets 1))
+              this)
+            (< y-rank x-rank)
+            (do
+              (assoc! elt-map y-root (assoc (get elt-map y-root) :parent x-root))
+              (set! num-sets (- num-sets 1))
+              this)
+            :else
+            (do
+              (assoc! elt-map y-root (assoc (elt-map y-root) :parent x-root))
+              (assoc! elt-map x-root (assoc (elt-map x-root) :rank (inc x-rank)))
+              (set! num-sets (- num-sets 1))
+              this)))))
 
 (def ^:private empty-union-find (->PersistentDSF (ref {}) 0 {}))
 
