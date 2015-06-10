f1d316c4ac2172e25812ae0406e1a9d3e4ceeab2
Chris Nuernberger <chris@thinktopic.com>
2015-06-10 12:56:13 -0600

    Deploying to s3 bucket

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.jordanlewis/data.union-find "0.1.1"
+(defproject org.jordanlewis/data.union-find "0.1.1-thinktopic"
             :description "Persistent disjoint-set forests using Tarjan's union-find algorithm."
             :url "http://github.com/jordanlewis/data.union-find"
             :license {:name "Eclipse Public License"
@@ -7,4 +7,13 @@
 
             :profiles
             {:dev {:source-paths      ["src" "dev"]
-                   :dependencies [[criterium "0.4.3"]]}})
+                   :dependencies [[criterium "0.4.3"]]}}
+            :plugins [[s3-wagon-private "1.1.2"]]
+            :repositories  {"snapshots"  {:url "s3p://thinktopic.jars/snapshots/"
+                                :passphrase :env
+                                :username :env}
+                  "releases"  {:url "s3p://thinktopic.jars/releases/"
+                               :passphrase :env
+                               :username :env}}
+
+            )
